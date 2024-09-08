93ad6958569fc17429b53a13a0d84e1d85db568f
Tech-master1234 <157775440+Tech-master1234@users.noreply.github.com>
2024-09-08 15:40:27 +0530

    Update run.py

--- run.py
+++ run.py
@@ -4,5 +4,5 @@ from dbapplication.run import flask_app
 
 flask_app = create_app()
 
-if __name__ == '__main__':
-    flask_app.run(host='0.0.0.0',debug=True)
+#if __name__ == '__main__':
+#    flask_app.run(host='0.0.0.0',debug=True)
